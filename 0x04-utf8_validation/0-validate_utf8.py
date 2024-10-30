f24d6602c9d304a2a6d2df63fdf49de151fe09c1
root <mohammed2004koki@gmail.com>
2024-10-30 07:50:11 +0300

    bitmask

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,61 +15,18 @@ def validUTF8(data):
         Mask = 1 << 7
         i = 0
         while Mask & num:
-            Mask <<= 1
+            Mask >>= 1
             i += 1
         return i
-    for i in range(len(data)):
-        num = data[i] % 255
+
+    for index in range(len(data)):
+        num = data[index] % 256
         i = check(num)
         if i == 0:
             continue
-        if i == 1:
-            j = check(data[i + 1])
-            if j == 1:
-                continue
-            else:
+        for j in range(1, 10):
+            num = data[index + j] % 256
+            if not (check(num) == 1):
                 return False
-        elif i == 2:
-            j = check(data[i + 1])
-            if j == 1:
-                k = check(data[i + 2])
-                if k == 1:
-                    i = i + 1
-                else:
-                    return False
-            else:
-                return False
-        elif i == 3:
-            j = check(data[i + 1])
-            if j == 1:
-                k = check(data[i + 2])
-                if k == 1:
-                    last = check(data[i + 3])
-                    if last == 1:
-                        i = i + 2
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        elif i == 4:
-            j = check(data[i + 1])
-            if j == 1:
-                k = check(data[i + 2])
-                if k == 1:
-                    last = check(data[i + 3])
-                    if last == 1:
-                        m = check(data[i + 4])
-                        if m == 1:
-                            i = i + 3
-                        else:
-                            return False
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-
+        index += i - 2
     return True
