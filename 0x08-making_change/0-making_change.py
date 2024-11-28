6d11116fd851f78647a2178183095e6507ec3e8d
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-11-29 01:52:31 +0200

    test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,8 @@ def makeChange(coins, total):
     """
     make change of total from coins
     """
+    if total <= 0:
+        return 0
     db = [total + 1] * (total + 1)
     db[0] = 0
     for i in range(1, total + 1):
