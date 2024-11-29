0e61f5c548786b0f381b7c4e849327ed86998011
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-11-29 02:12:07 +0200

    final recursive

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,7 +9,7 @@ def makeChange(coins, total):
     Make change of total from coins using memoization
     """
     db = {}
-    
+
     def helper(n):
         if n in db:
             return db[n]
@@ -17,13 +17,13 @@ def makeChange(coins, total):
             return 0
         if n < 0:
             return 0
-        
+
         min_coins = n + 1
         for coin in coins:
             num_coins = helper(n - coin) + 1
             if num_coins < min_coins:
                 min_coins = num_coins
-        
+
         db[n] = min_coins
         return db[n]
 
