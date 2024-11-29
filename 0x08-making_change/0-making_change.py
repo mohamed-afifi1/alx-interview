53f0924eb492c5aeed1f49db7915f92ac618824e
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-11-29 02:16:52 +0200

    test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,6 +14,8 @@ def makeChange(coins, total):
     db[0] = 0
     for i in range(1, total + 1):
         for coin in coins:
-            if i - coin >= 0:
+            if total - coin >= 0:
                 db[i] = min(db[i], 1 + db[i - coin])
-    return db[total] if db[total] >= total else -1
+    if db[total] == total + 1:
+        return -1
+    return db[total]
