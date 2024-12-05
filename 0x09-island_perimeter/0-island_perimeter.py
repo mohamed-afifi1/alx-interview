6fdeea1714550ef53abecb83b526d8d492aed5a6
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-12-05 15:15:38 +0200

    test

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,12 +15,12 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 # Check the adjacent cells
-                if i > 0 and grid[i-1][j] == 0:
+                if i == 0 or grid[i-1][j] == 0:
                     perimeter += 1
-                if j > 0 and grid[i][j-1] == 0:
+                if j == 0 or grid[i][j-1] == 0:
                     perimeter += 1
-                if i < rows - 1 and grid[i+1][j] == 0:
+                if i == rows - 1 or grid[i+1][j] == 0:
                     perimeter += 1
-                if j < cols - 1 and grid[i][j+1] == 0:
+                if j == cols - 1 or grid[i][j+1] == 0:
                     perimeter += 1
     return perimeter
