fe6b3bf77de6b4fcec00365a7affcfbc626d02d7
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-12-12 20:03:18 +0200

    please finish

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -16,19 +16,18 @@ def isWinner(x, nums):
         Return all primes up to n
         """
         sieve = [True] * (n + 1)
-        for x in range(2, int(n**0.5) + 1):
+        res = 0
+        for x in range(2, n + 1):
             if sieve[x]:
+                res += 1
                 for i in range(x*x, n + 1, x):
                     sieve[i] = False
-        res = 0
-        for p in range(2, n + 1):
-            if sieve[p]:
-                res += p
         return res
+
     Ben = 0
     Maria = 0
-    for i in nums:
-        if primes(i) % 2 == 0:
+    for i in range(x):
+        if primes(nums[i]) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
